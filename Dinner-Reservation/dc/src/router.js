da7060a4785e0729cff5341000f4eee0d43ec01b
chen1003 <cxy820899452@163.com>
2019-08-11 20:11:52 +0800

    我的页面更新

--- Dinner-Reservation/dc/src/router.js
+++ Dinner-Reservation/dc/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Index from './views/Index'
 import Details from './views/Details'
 import Login from './views/Login'
+import Mine from './views/Mine'
 import Notfound from './views/Notfound'
 Vue.use(Router)
 
@@ -11,6 +12,7 @@ export default new Router({
     {path:"/",component:Index},
     {path:"/details",component:Details},
     {path:"/login",component:Login},
+    {path:"/mine",component:Mine},
     {path:"*",component:Notfound}
     
   ]
---
+++ Dinner-Reservation/dc/src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+   <div class="bg">
+      <!-- login.vue -->
+        <!-- 标题 -->
+        <div class="login_title">
+          <img src="../img/mine/left.png">
+          <p>登录</p>
+        </div>
+        <table></table>
+        <div>
+        <!-- 创建输入框 -->
+          <input class="uname" placeholder="输入手机号" v-model="uname" ><br>
+        <!-- 密码输入框 -->
+          <input class="upwd" label="密码"  placeholder="输入密码" type="password"  v-model="upwd"><span>|获取验证码</span>
+        </div>
+        <!-- 登录按钮 -->
+        <mt-button size="large" type="danger">登录</mt-button>
+        <div class="point">
+           <a href="#">邮件登录</a>
+           <a href="#">收不到验证码?</a>
+        </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  @import url("../assets/css/comm.css");
+  @import url("../assets/css/login.css");
+</style>
