5cebc701c81d7c09eb1838d25a95add7611c928a
cxy <csy@163.com>
2019-08-12 13:29:46 +0800

    更新

--- Dinner-Reservation/dc/src/views/Login.vue
+++ Dinner-Reservation/dc/src/views/Login.vue
@@ -4,7 +4,7 @@
         <!-- 标题 -->
         <div class="login_title">
           <img src="../img/mine/left.png">
-          <p>登录</p>
+          <p>登录</p> 
         </div>
         <table></table>
         <!-- 创建输入框 -->
@@ -13,7 +13,8 @@
         </div>
         <!-- 密码输入框 -->
         <div class="my_bgcolor">
-          <input class="upwd" label="密码"  placeholder="输入密码" type="password"  v-model="upwd"><span>|获取验证码</span>
+          <input class="upwd" label="密码"  placeholder="输入密码" type="password"  v-model="upwd">
+          <span>|&nbsp;&nbsp;获取验证码</span>
         </div>
         <!-- 登录按钮 -->
         <mt-button size="large" type="danger">登录</mt-button>
