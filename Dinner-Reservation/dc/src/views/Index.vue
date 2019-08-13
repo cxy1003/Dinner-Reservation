dd6a09ce8c6ae64b1e1850ed2b60de85d1fce8a6
ssy970605 <y15339165228@163.com>
2019-08-13 20:48:46 +0800

    index

--- Dinner-Reservation/dc/src/views/Index.vue
+++ Dinner-Reservation/dc/src/views/Index.vue
@@ -5,30 +5,30 @@
                 <div id="top-img">
                     <a class="font_family"  href="javascript:;">上海 </a>
                     <!-- icon V 图标 -->
-                    <img class="icon" src="../img/indexs/icon1.png" alt="">         
+                    <img class="icon" src="../../public/img/indexs/icon1.png" alt="">         
                     <!-- 文本框 -->
                         <!-- 搜索图片 -->
-                    <img class="search" src="../img/indexs/search.png" alt="">
+                    <img class="search" src="../../public/img/indexs/search.png" alt="">
                     <input type="text" class="inputIcon" placeholder="请输入商户名，地点或菜名">           
                 </div>
                 <!-- 附近############ -->
                 <div class="near">
-                    <img src="../img/indexs/near.png" alt="">
+                    <img src="../../public/img/indexs/near.png" alt="">
                     <p>附近</p>
                 </div>          
             </div>
         <!--餐厅预订 -->
             <ul class="perplot">
                 <li class="perplot-list">
-                        &nbsp;&nbsp;&nbsp;&nbsp;<img class="perplot-img"  src="../img/indexs/preplot1.png" alt="">
+                        &nbsp;&nbsp;&nbsp;&nbsp;<img class="perplot-img"  src="../../public/img/indexs/preplot1.png" alt="">
                         <p class="font">餐厅预订</p>
                     </li>
                     <li class="perplot-list">
-                        &nbsp;<img  class="perplot-img" src="../img/indexs/preplot2.png" alt="">
+                        &nbsp;<img  class="perplot-img" src="../../public/img/indexs/preplot2.png" alt="">
                         <p class="font">电话预定</p>
                     </li>
                     <li class="perplot-list">
-                        &nbsp;<img  class="perplot-img" src="../img/indexs/preplot.png" alt="">
+                        &nbsp;<img  class="perplot-img" src="../../public/img/indexs/preplot.png" alt="">
                         <p class="font">小秘预订</p>
                 </li>
             </ul>
@@ -40,27 +40,27 @@
                     <p class="font_family">推荐餐厅</p>
                 </div> 
                 <!--餐厅介绍  -->
-                <div class="info">
-                    <div class=a>
+                <div class="info" >
+                    <div class="a" v-for="(elem,i) of arr" :key="i">
+                        <img src="../img/indexs/g1.jpg" alt="">
+                        <p class="font_family" v-text="elem.store"></p>
+                        <div class="font_price">{{elem.store}}</div>
+                    </div>
+                    <!-- <div class="a">
                         <img src="../img/indexs/g1.jpg" alt="">
                         <p class="font_family">粤珍轩 土门商厦店</p>
                         <div class="font_price">人均:￥60-100</div>
                     </div>
-                    <div class=a>
+                    <div class="a">
                         <img src="../img/indexs/g1.jpg" alt="">
                         <p class="font_family">粤珍轩 土门商厦店</p>
                         <div class="font_price">人均:￥60-100</div>
                     </div>
-                    <div class=a>
+                    <div class="a">
                         <img src="../img/indexs/g1.jpg" alt="">
                         <p class="font_family">粤珍轩 土门商厦店</p>
                         <div class="font_price">人均:￥60-100</div>
-                    </div>
-                    <div class=a>
-                        <img src="../img/indexs/g1.jpg" alt="">
-                        <p class="font_family">粤珍轩 土门商厦店</p>
-                        <div class="font_price">人均:￥60-100</div>
-                    </div>
+                    </div> -->
                 </div>      
             </div>
  </div>
@@ -69,12 +69,14 @@
 export default {
     data() {
         return {
-            
+            arr:[],
+            // p1:"",
         }
     },
     created() {
         this.axios.get("http://127.0.0.1:5050/myindex/goods").then(result=>{
-            console.log(result)
+             var arr=result.data;
+             console.log(arr)
         })
     },
 };
