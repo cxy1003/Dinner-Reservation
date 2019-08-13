fd9bdae8e6c97127880b1a24fa05099db10f767d
sun-weiwei <wei15709287597@163.com>
2019-08-13 20:41:43 +0800

    update  details ajax

--- Dinner-Reservation/dc/src/views/Details.vue
+++ Dinner-Reservation/dc/src/views/Details.vue
@@ -15,11 +15,11 @@
         <div class="details font_family">
             <!-- 详情页图片部分 -->
             <div class="details_img">
-                <img src="../../public/img/details/d11.jpg" alt="">
+                <img :src="'../../public/img/details/'+p1.img" alt="">
             </div>
             <!-- 店家详情 -->
             <div class="d_title">
-                <p class="d_title_store">粤珍轩 土门商厦店</p>
+                <p class="d_title_store" v-text="p1.d_name"></p>
                 <div>
                     <div id="d1">
                         <img src="../../public/img/details/ss1.png" alt="">
@@ -27,24 +27,25 @@
                         <img src="../../public/img/details/ss1.png" alt="">
                         <img src="../../public/img/details/ss1.png" alt="">
                         <img src="../../public/img/details/ss1.png" alt="">
-                        <span class="font_span">¥60-100</span>
+                        <span class="font_span">¥</span>
+                        <span class="font_span" v-text="p1.price"></span>
                     </div>
                     <div>
-                        <span  class="font_span d-mp">粤菜馆</span>
+                        <span  class="font_span d-mp" v-text="p1.subtitle"></span>
                     </div>
                 </div>
-                <p class="font_span mb mt">口味 3.6 环境 3.6 服务 3.6</p>
+                <p class="font_span mb mt" v-text="p1.intr"></p>
             </div>
             <!-- 地址部分 -->
             <div class="adress">
                 <img src="../../public/img/details/ad.png" alt="">
-                <span class="font_span mr">丰镐东路509号 (土门十字东北角国美电器旁3楼)</span>
+                <span class="font_span mr" v-text="p1.address"></span>
                 <img src="../../public/img/details/phone.png" alt="">
             </div>
             <!-- 提示信息 -->
             <div class="d_info mb">
                 <img src="../../public/img/details/info.png" alt="">
-                <span class="font_span">餐厅分为中餐和火锅，预定时请告知</span>
+                <span class="font_span" v-text="p1.information"></span>
             </div>
             <!-- 优惠活动 -->
             <div class="d_actity">
@@ -53,7 +54,7 @@
                 </div>
                 <div>
                     <img src="../../public/img/details/ji.png" alt="">
-                    <span class="font_span l_r_padding">吃一元，积一分</span>
+                    <span class="font_span l_r_padding" v-text="p1.activity"></span>
                 </div>
             </div>
             <!-- 推荐菜 -->
@@ -81,13 +82,11 @@
                 </div>
                 <div class="t_b_padding bb">
                     <span class="font_price font_span">详情介绍:</span>
-                    <span class="l_r_padding font_span">
-                        粤珍轩，一家主打粤菜的餐厅。餐厅装修的很现代化。进入宽敞的大厅，首先进入眼帘的是整齐的桌椅。大厅可以提供200位客人同时就餐。进入包房,包房以暖色调为主，给人一种温暖的感觉。餐厅的台湾卤肉、榴莲酥、香酥肉饼、豆豉蒸排骨、酿三宝都很不错，值得一试。营业时间：10:00-22:00
-                    </span>
+                    <span class="l_r_padding font_span" v-text="p1.d_intr"></span>
                 </div>
                 <div class="t_b_padding">
                     <span class="font_price font_span">电话：</span>
-                    <span class="font_span">02157575777</span>
+                    <span class="font_span" v-text="p1.d_phone"></span>
                 </div>
             </div>
             <!-- 拍照和点评的功能模块 -->
@@ -127,7 +126,32 @@
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            p1:"",
+            p2:"",
+            p3:"",
+            p4:"",
+            p5:"",
+            p6:"",
+            p7:"",
+            p8:""
+        }
+    },
+    created() {
+        this.axios.get("http://127.0.0.1:5050/mydetails/details").then(result=>{
+           var [p1,p2,p3,p4,p5,p6,p7,p8]=result.data;
+           console.log(p1)
+           this.p1=p1;
+           this.p2=p2;
+           this.p3=p3;
+           this.p4=p4;
+           this.p5=p5;
+           this.p6=p6;
+           this.p7=p7;
+           this.p8=p8;
+        })
+    },
 }
 </script>
 <style>
