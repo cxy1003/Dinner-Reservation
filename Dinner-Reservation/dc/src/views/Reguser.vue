c118399c0e2d8d28b70dfb2f471f413fbe236a3d
chen1003 <cxy820899452@163.com>
2019-08-14 01:05:04 +0800

    底部导航控制面板更新

--- Dinner-Reservation/dc/src/views/Reguser.vue
+++ Dinner-Reservation/dc/src/views/Reguser.vue
@@ -1,28 +1,21 @@
 <template>
-   <div class="bg">
+   <div class="my_bgcolor">
       <!-- login.vue -->
         <!-- 标题 -->
         <div class="login_title">
           <img src="../img/mine/left.png">
-          <p></p>
+          <p>注册</p>
         </div>
         <table></table>
         <!-- 创建输入框 -->
-        <div class="my_bgcolor">
-          <input class="uname" placeholder="输入手机号" v-model="uname" >
-        </div>
-        <div class="my_bgcolor">
-          <input class="uname" placeholder="输入" v-model="uname" >
-        </div>
-        <div class="my_bgcolor">
-          <input class="uname" placeholder="输入手机号" v-model="uname" >
-        </div>
-        <!-- 密码输入框 -->
-        <div class="my_bgcolor">
-          <input class="upwd" label="密码"  placeholder="输入密码" type="password"  v-model="upwd"><span>|获取验证码</span>
-        </div>
+       <mt-field  class="personal_infor" label="用户名" placeholder="请输入用户名" v-model="uname">
+         <img slot="icon" src="../img/mine/dianhua.png" width="24" height="24">
+       </mt-field>
+       <mt-field class="personal_infor" label="手机号" placeholder="请输入手机号" type="email" v-model="email"></mt-field>
+       <mt-field class="personal_infor" label="用户密码" placeholder="请输入密码" type="password" v-modal="password"></mt-field>
+       <mt-field class="personal_infor" label="确认密码" placeholder="请再次输入密码" type="tel" v-model="phone"></mt-field>
         <!-- 登录按钮 -->
-        <mt-button size="large" type="danger">注册</mt-button>
+        <mt-button size="large" type="danger">提交</mt-button>
         <div class="point">
            <a href="#">邮件登录</a>
            <a href="#">收不到验证码?</a>
@@ -36,6 +29,5 @@ export default {
 </script>
 
 <style>
-  @import url("../assets/css/comm.css");
   @import url("../assets/css/login.css");
 </style>
---
+++ Dinner-Reservation/dc/src/views/Tablebar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- <h1>TabBarIcon.vue</h1> -->
+        <!-- 1:显示一张图片 -->
+        <!-- 如果当前选中状态 显示 选中图片 -->
+        <!-- 如果当前默认状态显示  默认图片 -->
+       <img  :src="focused?selectedImage:normalImage" class="imgstyle"> 
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        focused:false,//状态默认 false
+        selectedImage:{default:""},//选中图片
+        normalImage:{default:""} //默认图片
+    }
+}
+</script>
+<style scoped>
+  .imgstyle{
+      width:30px;
+      height:30px;
+  }
+</style>
