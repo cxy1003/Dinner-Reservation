0e66b937f2fe26fecc7952b16f497607c24bad07
sun-weiwei <wei15709287597@163.com>
2019-08-14 11:16:03 +0800

    预订页面的更新

--- Dinner-Reservation/dc/src/views/Userpreplot.vue
+++ Dinner-Reservation/dc/src/views/Userpreplot.vue
@@ -1,39 +1,52 @@
 <template>
-    <div class="u_preplot bg">
+    <div class="u_preplot bg l_r_padding">
         <!-- 餐厅预订订单页面 -->
         <div class="u_preplot_yu">
             <!-- 导航部分 -->
             <img src="../../public/img/details/return.png" alt="">
             <span class="font_family">餐厅详情</span>
         </div>
-        <div class="mt">
-            <div>
-                <span>人数</span>
-                <span>4人</span>
+        <div class="mt u_info bb">
+            <div class="bb t_b_padding">
+                <span class="font_family u_bg_margin">人数</span>
+                <span class="font_family">4人&gt;</span>
             </div>
-            <span>时间</span>
-            <span>请选择就餐时间</span>
+            <div class="t_b_padding ">
+                <span class="font_family u_b_margin">时间</span>
+                <span class="font_family ">请选择就餐时间&gt;</span>
+            </div>
         </div>
         <!-- 预订包房 -->
-        <div>
+        <div class="u_switch bt bb t_b_padding">
             <span>我要预订包房</span>
             <!-- 开关 -->
-            <mt-switch type="" size="small"></mt-switch>
+            <mt-switch  size="small"></mt-switch>
         </div>
         <!-- 用户信息 -->
-        <div>
-            <!-- <mt-radio value="男士"></mt-radio> -->
-            <!-- <mt-input></mt-input> -->
-            <span>电话</span>
+        <div class="">
+            <div class="u_radio mb">
+                <input type="radio"  name="sex" class="mint-radio-core">男
+                <input type="radio" name="sex" class="mint-radio-core">女
+            </div>
+            <div class=" bt bb t_b_padding">
+                <span>18720569325</span>
+            </div>
         </div>
-        <div>
+        <!-- 需求安排 -->
+        <div class="u_anpai bb">
             <input type="text" placeholder="如有附加需求，请填写，餐厅将尽量安排">
         </div>
-        <div>
+        <div class="u_switch bt bb t_b_padding">
             <span>替他人订餐</span>
+            <!-- 开关 -->
+            <mt-switch  size="small"></mt-switch>
         </div>
-        <mt-button type="danger" size="small">确定提交</mt-button>
+
+        <div class="u_end  bb">
+            <mt-button type="danger" size="small" align="center">确定提交</mt-button>
+        </div>
     </div>
+    
 </template>
 <script>
     export default {
@@ -41,5 +54,5 @@
     };
 </script>
 <style>
-
+    @import url("../assets/css/userpreplot.css");
 </style>
