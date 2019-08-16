6d629748a86125930571a3a01b91369fbc97453d
sun-weiwei <wei15709287597@163.com>
2019-08-16 11:25:37 +0800

    注册页面更改

--- Dinner-Reservation/dc/src/views/Reguser.vue
+++ Dinner-Reservation/dc/src/views/Reguser.vue
@@ -10,9 +10,9 @@
        <mt-field  class="personal_infor" label="用户名" placeholder="请输入用户名" v-model="uname">
          <img slot="icon" src="../img/mine/dianhua.png" width="24" height="24">
        </mt-field>
-       <mt-field class="personal_infor" label="手机号" placeholder="请输入手机号"  v-model="phone"></mt-field>
-       <mt-field class="personal_infor" label="用户密码" placeholder="请输入密码" type="password" v-modal="upwd"></mt-field>
-       <mt-field class="personal_infor" label="确认密码" placeholder="请再次输入密码" type="password" v-model="reupwd"></mt-field>
+       <mt-field class="personal_infor" label="手机号" placeholder="请输入手机号" type="email" v-model="phone"></mt-field>
+       <mt-field class="personal_infor" label="用户密码" placeholder="请输入密码" type="password" v-model="upwd"></mt-field>
+       <mt-field class="personal_infor" label="确认密码" placeholder="请再次输入密码" type="tel" v-model="reupwd"></mt-field>
         <!-- 登录按钮 -->
         <mt-button size="large" type="danger" @click="userReg">提交</mt-button>
         <div class="point">
@@ -25,43 +25,56 @@ export default {
  data(){
    return{
      uname:"",
-     phone:"",
-     upwd:"",
+     phone:null,
+     opwd:"",
      reupwd:"",
+     upwd:""
    }
  },
  methods:{
    userReg(){
-      var reg=/^[a-z0-9]{3,11}$/i;
+      var ureg=/^[a-z0-9]{3,6}$/i;
+      var preg=/^[a-z0-9]{3,6}$/i;
+      var phreg=/^1[3-7][0-9]{9}$/;
       var uname=this.uname;
       var upwd=this.upwd;
       var phone=this.phone;
-      var reupwd;
+      var reupwd=this.reupwd;
     // 用户名验证 如果格式不正确提示错误信息
-    if(!reg.test(uname)){
+    if(!ureg.test(uname)){
       this.$messagebox("用户名格式不正确");
       return;
-    }else if(reg.test(upwd)){
+    }else if(!preg.test(upwd)){
       this.$messagebox("密码格式不正确");
       return;
-    }else if(!reg.test(phone)){
+    }else if(!phreg.test(phone)){
       this.$messagebox("手机格式不正确");
       return;
-    }else if(reupwd!=upwd){
+    }else if(!(upwd==reupwd)){
       this.$messagebox("两次输入密码不一致");
       return;
+    }else{
+      this.$messagebox("注册成功").then(result=>{
+        var url = "http://127.0.0.1:5050/user/reg";
+        var obj = {uname:uname,upwd:upwd,phone:phone};
+        this.axios.post(url,{params:obj}).then(res=>{
+          console.log(res)
+        })
+        this.$router.push("/Login")
+      })
+       
     }
-    //发送ajax请求 axios
-    var url = "user/reg";
-    var obj = {uname:uname,upwd:upwd,phone:phone};
-    this.axios.post(url,{params:obj}).then(res=>{
-      if(res.data.code==200){
-        this.$toast("注册成功");
-        this.$router.push("/Mine");
-      }else{
-        this.$messagebox("用户名或密码不正确");
-      }
-    })
+    // 发送ajax请求 axios
+    // var url = "http://127.0.0.1:5050/user/reg";
+    // var obj = {uname:uname,upwd:upwd,phone:phone};
+    // this.axios.post(url,{params:obj}).then(res=>{
+    //   if(res.data.code==200){
+    //     this.$toast("登陆成功");
+    //     this.$router.push("/Mine");
+    //   }else{
+    //     this.$messagebox("用户名或密码不正确");
+    //   }
+    // })
    },
    go(){
      this.$router.push("/Login")
